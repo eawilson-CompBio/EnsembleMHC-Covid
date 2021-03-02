e8c9075a130142aaecc5922e528af6c487a2784c
Eric Wilson <eawilso6@asu.edu>
2021-03-02 11:02:42 -0700

    update correlation data

--- update_new_data/update_github_image.sh
+++ update_new_data/update_github_image.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+git pull origin master
 
+LAST_RUN=$(cat last_date_run.txt)
 
-mv todays_data/* previous_days/
+mv todays_data/* previous_days/$LAST_RUN.png
 
 Rscript update_cor_data.R
 
