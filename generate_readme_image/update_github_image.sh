24cfc8ffbb5ddb882a58b41fa0d263500a245066
Eric Wilson <eawilso6@asu.edu>
2021-03-04 10:11:10 -0700

    update correlation data

--- generate_readme_image/update_github_image.sh
+++ generate_readme_image/update_github_image.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+source /Users/eawilso6/.bash_profile
+
 git pull origin master
 
 LAST_RUN=$(cat last_date_run.txt)
 
 mv todays_data/* previous_days/$LAST_RUN.png
 
-Rscript update_cor_data.R
+/usr/local/bin/Rscript update_cor_data.R
 
 cd ../
 
 git add generate_readme_image/
 git commit -m "update correlation data"
+
 git push origin master
